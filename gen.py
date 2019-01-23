89e76a4a9d0df59a96600e30c9d9b6113b0c928a
gosen <agusmdev@gmail.com>
2019-01-23 01:17:45 -0300

    Upgrade generator to work faster using multiprocess

--- gen.py
+++ gen.py
@@ -1,6 +1,7 @@
+import csv
 from random import randint
+from multiprocessing import Process, Pool
 
-isbn_list = []
 
 
 def isValidISBN(isbn):
@@ -8,32 +9,65 @@ def isValidISBN(isbn):
     # check for length
     if len(isbn) != 10:
         return False
+
+    formatted_num = isbn.replace("-", "")
+
     """
     Computing weighted sum
     of first 9 digits
     """
-    _sum = 0
+    checksum = 0
     for i in range(9):
-        digit = int(isbn[i])
-        _sum += digit * (10 - i)
+        digit = int(formatted_num[i])
+        checksum += digit * (10 - i)
+
     """
     If last digit is 'X', add
     10 to sum, else add its value.
     """
-
-    _sum += 10 if isbn[9] == 'X' else int(isbn[9])
+    last = formatted_num[-1]
+    checksum += 10 if last == 'X' else int(last)
     """
     Return true if weighted sum of
     digits is divisible by 11
     """
-    return (_sum % 11 == 0)
+    return (checksum % 11 == 0)
 
 
-for i in range(9999999999):
-    isbn = str(i)
-    isbn_ = "0"*(10 - len(isbn)) + isbn
-    isbn_X = "0"*(9 - len(isbn)) + isbn + "X"
-    if isValidISBN(isbn_):
-        isbn_list.append(isbn_)
-    if isValidISBN(isbn_X):
-        isbn_list.append(isbn_X)
+csvfile = "./isbns.txt"
+
+
+def multiprocess(limits=[]):
+    start = limits[0]
+    stop = limits[1]
+    for i in range(start, stop):
+        isbn = str(i)
+        isbn_ = "0"*(10 - len(isbn)) + isbn
+        isbn_X = "0"*(9 - len(isbn)) + isbn + "X"
+        if isValidISBN(isbn_):
+            isbn_list.append(isbn_)
+        if isValidISBN(isbn_X):
+            isbn_list.append(isbn_X)
+    return isbn_list
+
+
+if __name__ == '__main__':
+    isbn_list = []
+    args = [
+            [0, 10000000],
+            [10000000, 20000000],
+            [20000000, 30000000],
+            [30000000, 40000000],
+            [40000000, 50000000],
+            [60000000, 70000000],
+            [70000000, 80000000],
+            [80000000, 90000000],
+            [90000000, 99999999],
+            ]
+    p = Pool(9)
+    isbn_list += p.map(multiprocess, [*args])
+
+    with open(csvfile, 'w') as resultFile:
+        for i in isbn_list:
+            for j in i:
+                resultFile.write(j + ",")
